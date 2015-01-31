0c98d0da1f3c12e0aa55de2d9468490ebb041ebb
Corey Norris <coreypnorris@gmail.com>
2015-01-31 13:31:44 -0800

    chapter 4 progress

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 This is a practice app I built while following the [CodeSchool Angular.js tutorial](http://campus.codeschool.com/courses/shaping-up-with-angular-js)
+
+To run this app on your own:
+* clone the repo
+* Navigate to the app directory of the repo in your terminal
+* Start a simple server by running the `python -mSimpleHTTPServer` command in your terminal
+* Navigate to http://localhost:8000/index.html in a browser
--- app/js/app.js
+++ app/js/app.js
@@ -1,107 +1,85 @@
 (function(){
   var app = angular.module('gemStore', []);
 
-  var gems = [{
-    name: 'Azurite',
-    description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
-    shine: 8,
-    price: 110.50,
-    rarity: 7,
-    color: '#CCC',
-    faces: 14,
-    images: [
-      "images/gem-02.gif",
-      "images/gem-05.gif",
-      "images/gem-09.gif"
-    ],
-    reviews: [{
-      stars: 5,
-      body: "I love this gem!",
-      author: "joe@example.org",
-      createdOn: 1397490980837
-    }, {
-      stars: 1,
-      body: "This gem sucks.",
-      author: "tim@example.org",
-      createdOn: 1397490980837
-    }]
-  }, {
-    name: 'Bloodstone',
-    description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
-    shine: 9,
-    price: 22.90,
-    rarity: 6,
-    color: '#EEE',
-    faces: 12,
-    images: [
-      "images/gem-01.gif",
-      "images/gem-03.gif",
-      "images/gem-04.gif",
-    ],
-    reviews: [{
-      stars: 3,
-      body: "I think this gem was just OK, could honestly use more shine, IMO.",
-      author: "JimmyDean@example.org",
-      createdOn: 1397490980837
-    }, {
-      stars: 4,
-      body: "Any gem with 12 faces is for me!",
-      author: "gemsRock@example.org",
-      createdOn: 1397490980837
-    }]
-  }, {
-    name: 'Zircon',
-    description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
-    shine: 70,
-    price: 1100,
-    rarity: 2,
-    color: '#000',
-    faces: 6,
-    images: [
-      "images/gem-06.gif",
-      "images/gem-07.gif",
-      "images/gem-08.gif"
-    ],
-    reviews: [{
-      stars: 1,
-      body: "This gem is WAY too expensive for its rarity value.",
-      author: "turtleguyy@example.org",
-      createdOn: 1397490980837
-    }, {
-      stars: 1,
-      body: "BBW: High Shine != High Quality.",
-      author: "LouisW407@example.org",
-      createdOn: 1397490980837
-    }, {
-      stars: 1,
-      body: "Don't waste your rubles!",
-      author: "nat@example.org",
-      createdOn: 1397490980837
-    }]
-  }];
+  var gems = [
+      {
+        name: 'Azurite',
+        description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
+        shine: 8,
+        price: 110.50,
+        rarity: 7,
+        color: '#CCC',
+        faces: 14,
+        images: [
+          "images/gem-02.gif",
+          "images/gem-05.gif",
+          "images/gem-09.gif"
+        ],
+        reviews: [{
+          stars: 5,
+          body: "I love this gem!",
+          author: "joe@example.org"
+        }, {
+          stars: 1,
+          body: "This gem sucks.",
+          author: "tim@example.org"
+        }]
+      }, {
+        name: 'Bloodstone',
+        description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
+        shine: 9,
+        price: 22.90,
+        rarity: 6,
+        color: '#EEE',
+        faces: 12,
+        images: [
+          "images/gem-01.gif",
+          "images/gem-03.gif",
+          "images/gem-04.gif"
+        ],
+        reviews: [{
+          stars: 3,
+          body: "I think this gem was just OK, could honestly use more shine, IMO.",
+          author: "JimmyDean@example.org"
+        }, {
+          stars: 4,
+          body: "Any gem with 12 faces is for me!",
+          author: "gemsRock@example.org"
+        }]
+        }, {
+          name: 'Zircon',
+          description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+          shine: 70,
+          price: 1100,
+          rarity: 2,
+          color: '#000',
+          faces: 6,
+          images: [
+            "images/gem-06.gif",
+            "images/gem-07.gif",
+            "images/gem-08.gif"
+          ],
+          reviews: [{
+            stars: 1,
+            body: "This gem is WAY too expensive for its rarity value.",
+            author: "turtleguyy@example.org"
+          }, {
+            stars: 1,
+            body: "BBW: High Shine != High Quality.",
+            author: "LouisW407@example.org"
+          }, {
+            stars: 1,
+            body: "Don't waste your rubles!",
+            author: "nat@example.org"
+          }]
+      }];
+
+  ///////////////
+  // Controllers
+  //////////////
 
   app.controller('StoreController', function(){
     this.products = gems;
-  });
-
-  app.controller('GalleryController', function(){
-    this.current = 0;
-
-    this.setCurrent = function(newGallery){
-      this.current = newGallery || 0;
-    };
-  });
-
-  app.controller('TabController', function(){
-    this.tab = 1;
-
-    this.setTab = function(setTab) {
-      this.tab = setTab;
-    };
-
-    this.isSet = function(givenTab) {
-      return this.tab === givenTab;
-    };
   });
 
   app.controller('ReviewController', function(){
@@ -111,6 +89,57 @@
       this.review.createdOn = Date.now();
       product.reviews.push(this.review);
       this.review = {};
+    };
+  });
+
+  //////////////
+  // Directives
+  /////////////
+
+  app.directive("productGallery", function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'product-gallery.html',
+      controllerAs: 'gallery',
+      controller: function() {
+        this.current = 0;
+        this.setCurrent = function(imageNumber){
+          this.current = imageNumber || 0;
+        };
+      }
+    };
+  });
+
+  app.directive('productDescription', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'product-description.html'
+    };
+  });
+
+    app.directive("productSpecs", function() {
+    return {
+      restrict: 'A',
+      templateUrl: "product-specs.html"
+    };
+  });
+
+  app.directive("productTabs", function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'product-tabs.html',
+      controllerAs: 'tab',
+      controller: function(){
+        this.tab = 1;
+
+        this.isSet = function(checkTab) {
+          return this.tab === checkTab;
+        };
+
+        this.setTab = function(setTab) {
+          this.tab = setTab;
+        };
+      }
     };
   });
 })();
